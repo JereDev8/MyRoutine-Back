af709c47e4f71242bcf43f4cf70d0a2daff70975
Jeremias Quinteros <quinterosjeremias8@gmail.com>
2024-07-23 16:42:46 -0300

    commit de cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ dotenv.config()
 const server = express();
 
 server.use(cors({
-    origin: 'https://my-routine-two.vercel.app/', 
+    origin: 'https://my-routine-two.vercel.app', 
   }))
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
